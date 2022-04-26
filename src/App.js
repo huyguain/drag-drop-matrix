090b0383316dd5333019f988d858abc87a3a04ef
huyguainnt <huyguainnt@gmail.com>
2022-04-26 20:44:52 +0700

    done feature

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import './App.css'
+import { useCallback, useState } from 'react'
+import Cell from 'components/Cell'
+
+function App() {
+    const [matrix, setMatrix] = useState([])
+    const [itemDrag, setItemDrag] = useState()
+    const [nount, setNount] = useState(null)
+
+    const genMatrix = () => {
+        const arrMatrix = []
+        let count = 1
+        for (let i = 0; i < nount; i++) {
+            arrMatrix[i] = []
+        }
+        for (let j = 0; j < nount; j++) {
+            if (j % 2 === 0) {
+                for (let i = 0; i < nount; i++) {
+                    arrMatrix[i][j] = count
+                    count++
+                }
+            } else {
+                for (let i = nount - 1; i > -1; i--) {
+                    arrMatrix[i][j] = count
+                    count++
+                }
+            }
+        }
+        setMatrix(arrMatrix)
+    }
+
+    const drag = useCallback((ev, index) => {
+        ev.dataTransfer.effectAllowed = 'move'
+        setItemDrag(index)
+    }, [])
+
+    const allowDrop = useCallback(ev => {
+        ev.preventDefault()
+    }, [])
+
+    const drop = useCallback(
+        (ev, index) => {
+            ev.preventDefault()
+            if (!itemDrag) return
+            ;[matrix[index[0]][index[1]], matrix[itemDrag[0]][itemDrag[1]]] = [
+                matrix[itemDrag[0]][itemDrag[1]],
+                matrix[index[0]][index[1]],
+            ]
+            setMatrix([...matrix])
+        },
+        [itemDrag, matrix],
+    )
+
+    const handleTextInput = ev => {
+        if (ev.key === 'Enter' && nount > 0) {
+            genMatrix()
+        }
+    }
+
+    return (
+        <div className="App">
+            <h1>Zigzag pattern number table</h1>
+            <div className="input-text">
+                Nhập N (rôi nhấn Enter):{' '}
+                <input
+                    type="number"
+                    value={nount}
+                    onKeyDown={handleTextInput}
+                    onChange={ev => setNount(ev.target.value)}
+                />
+            </div>
+            {matrix.map((row, rowIndex) => {
+                return (
+                    <div style={{ display: 'flex' }} key={rowIndex}>
+                        {row.map((number, colIndex) => {
+                            // return <div
+                            //   key={colIndex}
+                            //   className='number-item'
+                            //   draggable
+                            //   onDragStart={ev => drag(ev, [rowIndex, colIndex])}
+                            //   onDragOver={allowDrop}
+                            //   onDrop={ev => drop(ev, [rowIndex, colIndex])}
+                            // >
+                            //   {number}
+                            // </div>
+                            return (
+                                <Cell
+                                    key={`${rowIndex}${colIndex}`}
+                                    number={number}
+                                    rowIndex={rowIndex}
+                                    colIndex={colIndex}
+                                    draggable
+                                    drag={drag}
+                                    allowDrop={allowDrop}
+                                    drop={drop}
+                                />
+                            )
+                        })}
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Cell/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './index.css'
+
+export default function Cell({
+    number,
+    rowIndex,
+    colIndex,
+    drag,
+    allowDrop,
+    drop,
+}) {
+    return (
+        <div
+            key={colIndex}
+            className="number-item"
+            draggable
+            onDragStart={ev => drag(ev, [rowIndex, colIndex])}
+            onDragOver={allowDrop}
+            onDrop={ev => drop(ev, [rowIndex, colIndex])}
+        >
+            {number}
+        </div>
+    )
+}
+
+Cell.propTypes = {
+    number: PropTypes.number.isRequired,
+    rowIndex: PropTypes.number.isRequired,
+    colIndex: PropTypes.number.isRequired,
+    drag: PropTypes.func.isRequired,
+    allowDrop: PropTypes.func.isRequired,
+    drop: PropTypes.func.isRequired,
+}
